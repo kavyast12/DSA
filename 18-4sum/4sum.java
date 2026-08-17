5cb31fb931fabbf5f13a6e252117f5d8917a9901
kavyast12 <kavyast45@gmail.com>
2026-08-17 13:43:31 +0530

    Time: 20 ms (66.15%) | Memory: 45.7 MB (63.38%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -7,7 +7,7 @@ class Solution {
             if(i>0 && nums[i]==nums[i-1])
                 continue;
             for(int j=i+1;j<n;j++){
-                if(j>i+1 && nums[j]==nums[j-1])
+                if(j!=i+1 && nums[j]==nums[j-1])
                     continue;
                 int left=j+1;int right =n-1;
                 while(left<right){
