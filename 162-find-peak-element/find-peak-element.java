96a2d6b32ed4c21a6e68b0638a8a6d93b33dcd0c
kavyast12 <kavyast45@gmail.com>
2026-08-31 23:17:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (88.33%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -19,11 +19,8 @@ class Solution {
             else if(nums[mid]>nums[mid-1]){
                 low=mid+1;
             }
-            else if(nums[mid]>nums[mid+1]){
-                high=mid-1;
-            }
             else{
-                low=mid+1;
+                high=mid-1;
             }
         }
         return -1;
