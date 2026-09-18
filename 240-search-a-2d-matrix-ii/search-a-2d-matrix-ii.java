caf800778111600ce1feccb3d8b505d3d596bc3d
kavyast12 <kavyast45@gmail.com>
2026-09-18 21:22:03 +0530

    Time: 4 ms (20.73%) | Memory: 48.2 MB (45.19%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,14 +1,26 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-                
+      for(int i=0;i<matrix.length;i++){
+        if(bs(matrix[i],target)){
+            return true;
+        }
+      }
+      return false;
+    }
+    public static boolean bs(int[] nums,int target){
+        int low=0;int high=nums.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                return true;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
         return false;
-        
     }
 }
